e21c6b0e6e6d5f9e5f4edf1785aeb471d980c480
Z User <z@container>
2026-08-24 20:47:48 +0000

    c5bb8af5-26e8-4c9d-bed4-31f45459fb77

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db = globalForPrisma.prisma ?? (globalForPrisma.prisma = new PrismaClient())
+function createPrismaClient(): PrismaClient {
+  const databaseUrl = process.env.DATABASE_URL || ''
+
+  if (databaseUrl.startsWith('libsql://')) {
+    const libsql = createClient({ url: databaseUrl })
+    const adapter = new PrismaLibSQL(libsql)
+    return new PrismaClient({
+      adapter,
+      datasources: { db: { url: 'file:./dev.db' } },
+    })
+  }
+
+  return new PrismaClient()
+}
+
+export const db = globalForPrisma.prisma ?? (globalForPrisma.prisma = createPrismaClient())
