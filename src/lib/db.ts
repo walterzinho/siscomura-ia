54f06540aec33fd1ce153321916f0acfa114d2ed
Z User <z@container>
2026-08-24 20:46:20 +0000

    1e7bf2a3-6cf2-428e-8d1a-44b7b1335ce4

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,22 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-function createPrismaClient(): PrismaClient {
-  const databaseUrl = process.env.DATABASE_URL || ''
-
-  if (databaseUrl.startsWith('libsql://')) {
-    const libsql = createClient({ url: databaseUrl })
-    const adapter = new PrismaLibSQL(libsql)
-    return new PrismaClient({ adapter })
-  }
-
-  // SQLite local (desarrollo)
-  return new PrismaClient()
-}
-
-export const db = globalForPrisma.prisma ?? (globalForPrisma.prisma = createPrismaClient())
+export const db = globalForPrisma.prisma ?? (globalForPrisma.prisma = new PrismaClient())
